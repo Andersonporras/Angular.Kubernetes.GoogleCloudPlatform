e7b7bd54a2e85dc77abf8f31409a9000e305dd18
Andersonporras <68761086+Andersonporras@users.noreply.github.com>
2026-06-25 11:24:03 -0500

    feat(ADD security components)
    add register users login component, and other security components

--- src/app/pages/auth/pages/login/login.component.ts
+++ src/app/pages/auth/pages/login/login.component.ts
@@ -11,26 +11,25 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loading$! : Observable<boolean | null>;
+  //loading$! : Observable<boolean | null>;
 
-  // constructor(
-  //   private store: Store<fromRoot.State>
-  // ) { }
+  constructor(
+   // private store: Store<fromRoot.State>
+  ) { }
 
   ngOnInit(): void {
 
   }
 
+  loginUsuario(form: NgForm): void {
+    // const userLoginRequest : fromUser.EmailPasswordCredentials = {
+    //   email: form.value.email,
+    //   password: form.value.password
+    };
 
-  // loginUsuario(form: NgForm): void {
-  //   const userLoginRequest : fromUser.EmailPasswordCredentials = {
-  //     email: form.value.email,
-  //     password: form.value.password
-  //   };
-
-  //   this.store.dispatch(new fromUser.SignInEmail(userLoginRequest));
+   // this.store.dispatch(new fromUser.SignInEmail(userLoginRequest));
 
 
-  // }
+
 
 }
--- src/app/pages/auth/pages/registration/registration.module.ts
+++ src/app/pages/auth/pages/registration/registration.module.ts
@@ -4,6 +4,16 @@ import { CommonModule } from '@angular/common';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+//import { SpinnerModule } from '@app/shared/indicators';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +21,18 @@ import { RegistrationComponent } from './registration.component';
   ],
   imports: [
     CommonModule,
-    RegistrationRoutingModule
+    RegistrationRoutingModule,
+
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule
+   // SpinnerModule
+
   ]
 })
 export class RegistrationModule { }
