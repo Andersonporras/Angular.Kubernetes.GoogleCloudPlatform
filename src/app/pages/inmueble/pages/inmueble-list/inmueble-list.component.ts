ccb7bcd46e5fbdb8858c81b4eec10e4976a39dc2
Andersonporras <68761086+Andersonporras@users.noreply.github.com>
2026-08-17 16:32:20 -0500

    feat(Add components to page)
    add news components to page and other things

--- src/app/pages/inmueble/pages/inmueble-list/inmueble-list.component.ts
+++ src/app/pages/inmueble/pages/inmueble-list/inmueble-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as fromRoot from '@app/store';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as fromList from '../../store/save';
+import { InmuebleResponse } from '../../store/save';
 
 @Component({
   selector: 'app-inmueble-list',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inmueble-list.component.scss']
 })
 export class InmuebleListComponent implements OnInit {
+  inmuebles$ ! : Observable<InmuebleResponse[] | null>
+  loading$ ! : Observable<boolean | null>
 
-  constructor() { }
+  pictureDefault : string = "https://firebasestorage.googleapis.com/v0/b/edificacion-app.appspot.com/o/image%2F1637099019171_O5986058_0.jpg?alt=media&token=0a146233-d63b-4702-b28d-6eaddf5e207a"
+
+
+ constructor(
+    private store: Store<fromRoot.State>
+  ) { }
 
   ngOnInit(): void {
+    this.store.dispatch(new fromList.Read());
+    this.loading$ = this.store.pipe(select(fromList.getLoading));
+    this.inmuebles$ = this.store.pipe(select(fromList.getInmuebles));
   }
 
 }
